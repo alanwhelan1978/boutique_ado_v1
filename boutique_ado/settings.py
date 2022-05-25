beb9e7033f7a81c5609e29320ea54fe2322981a9
alanwhelan1978 <alanwhelan1978@gmail.com>
2022-05-25 15:37:24 +0000

    fix placing of index.html folder

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -59,7 +59,6 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'templates', 'allauth'),
-
         ],
         'APP_DIRS': True,
         'OPTIONS': {
